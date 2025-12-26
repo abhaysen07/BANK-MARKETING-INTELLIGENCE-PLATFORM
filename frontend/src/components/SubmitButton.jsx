11ddc2a3f1bb39fc1e04f349579bf0538b26c126
Abhishek kumar <39051662+abhaysen07@users.noreply.github.com>
2025-12-26 16:29:48 +0530

    Delete frontend directory

--- frontend/src/components/SubmitButton.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/SubmitButton.jsx
-
-export default function SubmitButton({ loading }) {
-  return (
-    <button disabled={loading}>
-      {loading ? "Predicting..." : "Predict"}
-    </button>
-  );
-}
